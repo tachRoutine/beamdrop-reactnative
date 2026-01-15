29a61b0bfc314f13dfe975d8237b643be4cc9d96
tacheraSasi <tacherasasi@gmail.com>
2026-01-15 15:12:54 +0300

    Add Settings screen and route
    Wrap content in a SafeAreaView with placeholder ThemedText items and
    present
    the screen as a formSheet in RootLayout

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,14 +1,32 @@
-import { ThemedText } from '@/components/ThemedText'
-import { ThemedView } from '@/components/ThemedView'
-import React from 'react'
-
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Settings = () => {
   return (
-    <ThemedView>
-      <ThemedText>Settings</ThemedText>
-    </ThemedView>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemedView>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+        <ThemedText>Settings</ThemedText>
+      </ThemedView>
+    </SafeAreaView>
+  );
+};
 
-export default Settings
+export default Settings;
